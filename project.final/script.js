51094447fd0467380f46fd4601ada7d4b2a0e7e8
sfpietro-su <spietropaolo919@gmail.com>
2023-12-15 23:17:59 -0500

    final update

---
+++ project.final/script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("close").style.display = "inline";
+}
+function closeMenu() {
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("close").style.display = "none";
+}
