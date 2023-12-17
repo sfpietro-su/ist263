4565a8d47a5eeb5fa28f607c24e3e8252893e1e0
sfpietro-su <spietropaolo919@gmail.com>
2023-12-17 18:32:30 -0500

    slide show

--- project.final/script.js
+++ project.final/script.js
@@ -6,3 +6,17 @@ function closeMenu() {
     document.getElementById("nav").style.display = "none";
     document.getElementById("close").style.display = "none";
 }
+
+var myIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
--- project.working/script.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    document.getElementById("nav").style.display = "block";
-    document.getElementById("close").style.display = "inline";
-}
-function closeMenu() {
-    document.getElementById("nav").style.display = "none";
-    document.getElementById("close").style.display = "none";
-}
